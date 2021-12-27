4885648775977dfa4b72278c663e942f2ae99f26
Vasianandcarps <90087480+Vasianandcarps@users.noreply.github.com>
2021-12-27 10:53:59 +0200

    Add files via upload

---
+++ errors.js
@@ -0,0 +1,34 @@
+let a = prompt("input number a");
+let b = prompt("input number b");
+let c = prompt("input number c");
+let x;
+try {
+     
+     if (a==0) {
+        x = -c/b
+      } else if (b==0){
+          x=Math.sqrt(-c/a)
+      }else if (c==0){
+          x= 0;
+          x1= b/a;
+          alert(x1);
+      }else{
+          D= b*b - 4*a*c 
+          if (D<0){
+            throw new Error("D не может быть <0");
+          }else if (D==0){
+              x=-b/2*a;
+          }else{
+              x = -b + Math.sqrt(D) /2*a;
+              x1 = -b - Math.sqrt(D) /2*a;
+              alert(x1);
+          }
+      }
+      alert(x);
+     
+ } catch(e) {
+    alert("Извините, возникла ошибка: " + e.message);
+ } finally {
+    alert( "Пока!");
+ }
+
